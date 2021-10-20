9fa652cb97ea8649a415f12b1f0d439ff96c6205
Noyal Thomas <noyallthomas@gmail.com>
2021-10-20 09:40:10 +0530

    calculator

---
+++ script.js
@@ -0,0 +1,22 @@
+// function displayValue(num){
+//     let res=document.querySelector("#result");
+//     res.value+=num
+// }
+//or
+
+var displayValue=(num)=>result.value+=num
+
+// function clearBox(){
+//     result.value=""
+// }
+//or
+var clearBox=()=>result.value=""
+
+
+// function evaluateExpression(){
+//     let expr=result.value
+//     let out=eval(expr)
+//     result.value=out
+// }
+//or
+var evaluateExpression=()=>result.value=eval(result.value)
